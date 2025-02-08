8e88dcff1eecd4fdf156ec234f6251f93977fe5e
M3hTi <mehdinasir502@yahoo.com>
2025-02-08 15:44:34 +0330

    feat: add dark mode toggle and search form to Todo App

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,46 @@ import { CiSquarePlus, CiSquareMinus } from "react-icons/ci";
 import './App.css'
 
 function App() {
+  const [isDark, setIsDark] = React.useState(false)
+
+
+  React.useEffect(() => {
+    if(isDark){
+      document.body.classList.add('dark_mode')
+    }else{
+      document.body.classList.remove('dark_mode')
+    }
+  },[isDark])
+
+  const toggle = React.useCallback(() => setIsDark(state => !state),[])
   return (
     <>
-      <Header />
+      <Header isDark={isDark} onToggle={toggle} />
+      <SearchForm />
     </>
   )
 }
 
 
-function Header() {
+function Header({isDark, onToggle}) {
   return(
     <div className='header'>
       <h1>Todo App</h1>
-      <button className='toggle'></button>
+      <button className='btn pad-8' onClick={onToggle}>
+        {isDark ? 'Switch to Light mode ☀️': 'Switch to Dark mode 🌙'}
+      </button>
     </div>
+  )
+}
+
+function SearchForm(){
+  return(
+    <form className='form'>
+      <input type="text" placeholder="pls enter todo" />
+      <button type='submit'>
+      <CiSquarePlus />
+      </button>
+    </form>
   )
 }
 
